94e79b78b93e7a5efedf14a0bce7652257a66155
Anshu raj <Anshuraj6357@gmail.com>
2026-04-08 10:22:42 +0530

    updated

--- src/user/roomcard.jsx
+++ src/user/roomcard.jsx
@@ -154,7 +154,7 @@ const RoomCard = memo(function RoomCard({
                   </p>
 
                   <p className="text-sm font-black text-gray-900">
-                    ₹{room.category==="Pg" ? room.price : room.base_price}
+                    ₹{room.category==="Pg" ||room.category==="Rented-Room"? room.price : room.base_price}
                     <span className="text-[10px] text-gray-500">/mo</span>
                   </p>
                 </div>
